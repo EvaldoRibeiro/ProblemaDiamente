080988025b95b4e56f88a7955574a80aba396b2a
Evaldo Ribeiro <evardis@gmail.com>
2020-06-24 15:37:38 -0300

    Criado Projeto

---
+++ src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import device.ComboDevice;
+import device.ConcretePrinter;
+import device.ConcreteScanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		ConcretePrinter p = new ConcretePrinter("1080");
+		p.processDoc("My Letter");
+		p.print("My Letter");
+		
+		ConcreteScanner s = new ConcreteScanner ("2003");
+		p.processDoc("My Email");
+		System.out.println("Scan result: " + s.scan() );
+
+		ComboDevice c = new ComboDevice("2081");
+		c.processDoc("My dissertartion");
+		c.print("My dissertation");
+		System.out.println("Scan result: " + c.scan());
+	}
+
+}
